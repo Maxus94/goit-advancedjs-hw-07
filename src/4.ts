74e6a3d57529b2b68db0980b1dd10233a1e55682
Maxim <kmaxusk@gmail.com>
2023-12-06 19:58:39 +0200

    first

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,60 @@
-const key = new Key();
+class Key {
+    constructor(private signature: number) {
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+const key = new Key(Math.random());
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+const person = new Person(key);
+
+abstract class House {
+    tenants: Person[];
+    door: boolean = false;
+    key: Key;
+    constructor(key: Key){
+
+    }
+    public abstract openDoor(key: Key): void;
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person)
+         }
+    }
+}
+
+class MyHouse extends House{
+    constructor(key: Key){
+        super(key);
+    }
+    public openDoor(key: Key) {
+        if(key === this.key){
+        this.door = true;
+    }else{
+        this.door = false;
+    }
+    
+}}
+
 
 const house = new MyHouse(key);
-const person = new Person(key);
+
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
 
 
-export {};
+export { };
